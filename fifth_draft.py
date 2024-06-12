58adcbedb31a9d3ae72913f7196ba881df7bac52
Soumik Barua <67410061+BrokenSpaghetti02@users.noreply.github.com>
2024-06-12 17:53:26 +0800

    Update fifth_draft.py

--- fifth_draft.py
+++ fifth_draft.py
@@ -1,7 +1,7 @@
+import json
 import os
 import pyaudio
 import google.auth
-import json
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_vertexai import ChatVertexAI
 import sounddevice as sd
@@ -18,98 +18,61 @@ from langchain.prompts import (
 import time
 from google.cloud import speech
 
-#------------------------------------------------- Initialization and Definition -------------------------------------------------
+# Function to get credentials
+def get_credentials():
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/ubuntu/mini_pupper_bsp/demos/super_key.json"
+    credentials, project_id = google.auth.default()
 
-# Get credentials
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/ubuntu/mini_pupper_bsp/demos/super_key.json"
-credentials, project_id = google.auth.default()
+# Function to create all the instances
+def create_instances(RATE, CHUNK):
+    # Create model instance
+    model = ChatVertexAI(
+        model_name='gemini-pro',
+        convert_system_message_to_human=True,
+    )
 
-# Initialize model
-model = ChatVertexAI(
-    model_name='gemini-pro',
-    convert_system_message_to_human=True,
-)
+    # Create prompt instance
+    prompt = ChatPromptTemplate(
+        messages=[
+            SystemMessagePromptTemplate.from_template(
+                """
+                You are Megamind, a small male robo puppy. You will be a helpful AI assistant.
+                ---
+                NEVER USE emojis, hashtags, or asterisks.
+                ONLY answer in SHORT sentences.
+                """
+            ),
+            MessagesPlaceholder(variable_name="history"),
+            HumanMessagePromptTemplate.from_template("{input}"),
+        ]
+    )
 
-# Initialize prompt
-prompt = ChatPromptTemplate(
-    messages=[
-        SystemMessagePromptTemplate.from_template(
-            """
-            You are Jenny, a small female robo puppy. You will be a helpful AI assistant.
-            But your answers will always be short and never use emojis, hashtags, or asterisks.
-            Only answer in sentences.
-            """
-        ),
-        MessagesPlaceholder(variable_name="history"),
-        HumanMessagePromptTemplate.from_template("{input}"),
-    ]
-)
+    # Create memory instances
+    memory = ConversationBufferMemory(memory_key="history", return_messages=True)
 
-# Load conversation history from file
-def load_history(file_path):
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
-            return json.load(f)
-    return []
+    # Create Speech-to-Text instance
+    speech_client = speech.SpeechClient()
 
-# Save conversation history to file
-def save_history(file_path, history):
-    with open(file_path, 'w') as f:
-        json.dump(history, f, indent=4)
+    # Create TextToSpeechClient instance
+    tts_client = texttospeech.TextToSpeechClient()
 
-# Print conversation history
-def print_history(file_path):
-    history = load_history(file_path)
-    for message in history:
-        role = "User" if message['role'] == 'user' else "AI"
-        print(f"{role}: {message['content']}")
+    # Create PyAudio instance
+    p = pyaudio.PyAudio()
 
-# Define the file path for storing conversation history
-history_file_path = "/home/ubuntu/conversation_history.json"
+    # Create voice instance
+    voice = texttospeech.VoiceSelectionParams(
+            language_code="en-GB",
+            name="en-GB-Wavenet-B"
+        )
+    
+    # Create audio configuration instance
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.LINEAR16
+    )
 
-# Load the conversation history
-history = load_history(history_file_path)
+    return model, prompt, memory, speech_client, tts_client, p, voice, audio_config
 
-# Set memory for context
-memory = ConversationBufferMemory(memory_key="history", return_messages=True)
-
-# Populate memory with loaded history
-for message in history:
-    if message['role'] == 'user':
-        memory.chat_memory.add_user_message(message['content'])
-    elif message['role'] == 'ai':
-        memory.chat_memory.add_ai_message(message['content'])
-
-conversation = ConversationChain(llm=model, prompt=prompt, verbose=False, memory=memory)
-
-# Print the conversation history
-print("Conversation History:\n")
-print_history(history_file_path)
-
-# Create the Speech-to-Text instance
-speech_client = speech.SpeechClient()
-
-# Set the default speaker volume to maximum
-os.system("amixer -c 0 sset 'Headphone' 100%")
-
-# Create the TextToSpeechClient instance
-tts_client = texttospeech.TextToSpeechClient()
-
-def audio_generator(stream, chunk):
-    while True:
-        data = stream.read(chunk)
-        if not data:
-            break
-        yield data
-
-# Initialize PyAudio parameters
-RATE = 48000
-CHUNK = int(RATE / 10)  # 100ms chunks
-
-# Create the PyAudio instance
-p = pyaudio.PyAudio()
-
-def open_stream():
+def open_stream(RATE, CHUNK, p):
     return p.open(
         format=pyaudio.paInt16,
         channels=1,
@@ -118,9 +81,16 @@ def open_stream():
         frames_per_buffer=CHUNK
     )
 
-def detect_speech_and_transcribe():
-    print("Listening...")  # start listenning
-    requests = (speech.StreamingRecognizeRequest(audio_content=content) for content in audio_generator(stream, CHUNK))
+def audio_generator(stream, chunk):
+    while True:
+        data = stream.read(chunk)
+        if not data:
+            break
+        yield data
+
+# Function to detect voice and transribe speech
+def detect_speech_and_transcribe(stream, RATE, CHUNK, speech_client):
+    requests = (speech.StreamingRecognizeRequest(audio_content=content) for content in audio_generator(stream = stream, chunk = CHUNK))
     streaming_config = speech.StreamingRecognitionConfig(
         config=speech.RecognitionConfig(
             encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
@@ -141,102 +111,153 @@ def detect_speech_and_transcribe():
                     user_input = result.alternatives[0].transcript
                 return user_input
 
-print("Initialization Complete\n")
-time.sleep(1)
+# Load conversation history from file
+def load_history(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            return json.load(f)
+    return []
 
-speak = str(input("Speak to Mini Pupper? (y/n)\n"))
+# Save conversation history to file
+def save_history(file_path, history):
+    with open(file_path, 'w') as f:
+        json.dump(history, f, indent=4)
 
-while speak == 'n':
+# Print conversation history
+def print_history(file_path):
+    history = load_history(file_path)
+    for message in history:
+        role = "User" if message['role'] == 'user' else "AI"
+        print(f"{role}: {message['content']}")
+
+def main():
+    get_credentials()
+
+    # Set the default speaker volume to maximum
+    os.system("amixer -c 0 sset 'Headphone' 100%")
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    RATE = 48000
+    CHUNK = int(48000 / 10)
+    model, prompt, memory, speech_client, tts_client, p, voice, audio_config = create_instances(RATE = RATE, CHUNK = CHUNK)
+
+    # Define the file path for storing conversation history
+    history_file_path = "/home/ubuntu/conversation_history.json"
+
+    # Load the conversation history
+    history = load_history(history_file_path)
+
+    # Populate memory with loaded history
+    for message in history:
+        if message['role'] == 'user':
+            memory.chat_memory.add_user_message(message['content'])
+        elif message['role'] == 'ai':
+            memory.chat_memory.add_ai_message(message['content'])
+
+    conversation = ConversationChain(llm=model, prompt=prompt, verbose=False, memory=memory)
+
+    print("Initialization Complete!\n")
+    time.sleep(0.5)
+
     speak = str(input("Speak to Mini Pupper? (y/n)\n"))
 
-time.sleep(1)
+    while speak == 'n':
+        speak = str(input("Speak to Mini Pupper? (y/n)\n"))
 
-stream = open_stream()
+    time.sleep(0.5)
 
-while True:
+    stream = open_stream(RATE = RATE, CHUNK = CHUNK, p = p)
+
+    while True:
 
 #------------------------------------------------- Speech to Text -------------------------------------------------
 
-    # Record audio
-    print("Mini Pupper 2 is listening...\n")
-    user_input = detect_speech_and_transcribe()
-    print(f"Mini Pupper has stopped listening. User input: {user_input}\n")
+        print("Mini Pupper 2 is listening...\n")
+        user_input = detect_speech_and_transcribe(stream=stream, RATE = RATE, CHUNK = CHUNK, speech_client = speech_client)
+        print(f"Mini Pupper has stopped listening.\n")
+        print(f"User input: {user_input}\n")
+
+        while user_input == "":
+            speak = str(input("No user input. Speak to Mini Pupper? (y/n)\n"))
+
+            while speak == 'n':
+                speak = str(input("No user input. Speak to Mini Pupper? (y/n)\n"))
+
+            print("Mini Pupper 2 is listening...\n")
+            user_input = detect_speech_and_transcribe(stream=stream, RATE = RATE, CHUNK = CHUNK, speech_client = speech_client)
+            print(f"Mini Pupper has stopped listening.\n")
+            print(f"User input: {user_input}\n")
 
 #------------------------------------------------- Chat with Gemini Pro -------------------------------------------------
 
-    # Add the suffix to user input
-    user_input_with_suffix = user_input + " In short. NO asterisks, NO hashtags, or emojis. NO asterisks, NO hashtags, or emojis. "
+        # Fetch response from Gemini Pro
+        # Add the suffix to user input
+        user_input_with_suffix = user_input + " In short. NO asterisks, NO hashtags, or emojis. NO asterisks, NO hashtags, or emojis. "
 
-    # Fetch response from Gemini Pro
-    start_time = time.time()
-    result = conversation.invoke(input=user_input_with_suffix)['response']
-    end_time = time.time()
-    print(f"Gemini response time: {end_time - start_time:.2f} seconds\n")
+        start_time = time.time()
+        result = conversation.invoke(input=user_input_with_suffix + "In short. No asterisks, hashtags, or emojis.")['response']
+        end_time = time.time()
+        print(f"Gemini response time: {end_time - start_time:.2f} seconds\n")
 
-    print(f"Mini Pupper: {result}\n")
+        print(f"Mini Pupper: {result}\n")
 
-    # Update and save conversation history
-    memory.chat_memory.add_user_message(user_input_with_suffix)
-    memory.chat_memory.add_ai_message(result)
+        # Update and save conversation history
+        memory.chat_memory.add_user_message(user_input_with_suffix)
+        memory.chat_memory.add_ai_message(result)
 
-    # Generate the history to be saved
-    history = []
-    messages = memory.chat_memory.messages
-    for i in range(0, len(messages), 2):
-        if i < len(messages):
-            history.append({'role': 'user', 'content': messages[i].content})
-        if i + 1 < len(messages):
-            history.append({'role': 'ai', 'content': messages[i + 1].content})
+        # Generate the history to be saved
+        history = []
+        messages = memory.chat_memory.messages
+        for i in range(0, len(messages), 2):
+            if i < len(messages):
+                history.append({'role': 'user', 'content': messages[i].content})
+            if i + 1 < len(messages):
+                history.append({'role': 'ai', 'content': messages[i + 1].content})
 
-    # Save the updated history
-    save_history(history_file_path, history)
+        # Save the updated history
+        save_history(history_file_path, history)
 
-    # Set the text to be synthesized
-    text_to_speak = result
+        # Set the text to be synthesized
+        text_to_speak = result
 
 #------------------------------------------------- Text to Speech -------------------------------------------------
 
-    # Configure the synthesis request
-    synthesis_input = texttospeech.SynthesisInput(text=text_to_speak)
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="en-GB",
-        name="en-GB-Wavenet-F"
-    )
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.LINEAR16
-    )
+        # Configure the synthesis request
+        synthesis_input = texttospeech.SynthesisInput(text=text_to_speak)
 
-    start_time = time.time()
-    response = tts_client.synthesize_speech(
-        input=synthesis_input,
-        voice=voice,
-        audio_config=audio_config
-    )
-    end_time = time.time()
-    print(f"Text to Speech processing time: {end_time - start_time:.2f} seconds\n")
+        start_time = time.time()
+        response = tts_client.synthesize_speech(
+            input=synthesis_input,
+            voice=voice,
+            audio_config=audio_config,
+        )
+        end_time = time.time()
+        print(f"Text to Speech processing time: {end_time - start_time:.2f} seconds\n")
 
-    # Save the audio to a file
-    with open("/home/ubuntu/mini-pupper-2-output.wav", "wb") as out:
-        out.write(response.audio_content)
-        print(f'Audio content written to file "mini-pupper-2-output.wav\n"')
+        # Save the audio to a file
+        with open("/home/ubuntu/mini-pupper-2-output.wav", "wb") as out:
+            out.write(response.audio_content)
+            print(f'Audio content written to file "mini-pupper-2-output.wav"\n')
 
-    # Play audio
-    print("Mini Pupper 2 audio playback start...\n")
-    start_time = time.time()
-    audio, fs = sf.read("/home/ubuntu/mini-pupper-2-output.wav")
-    sd.play(audio, fs)
-    sd.wait()  # Wait for playback to finish
-    end_time = time.time()
-    print(f"Mini Pupper 2 audio playback end. Time taken: {end_time - start_time:.2f} seconds\n")
+        # Play audio
+        print("Mini Pupper 2 audio playback start...\n", end = '\r')
+        start_time = time.time()
+        audio, fs = sf.read("/home/ubuntu/mini-pupper-2-output.wav")
+        sd.play(audio, fs)
+        sd.wait()  # Wait for playback to finish
+        end_time = time.time()
+        print(f"Mini Pupper 2 audio playback end. Time taken: {end_time - start_time:.2f} seconds\n")
 
-    # Reset the stream for the next iteration
-    stream.stop_stream()
+        # Reset the stream for the next iteration
+        stream.stop_stream()
+        stream.close()
+        stream = open_stream(RATE = RATE, CHUNK = CHUNK, p = p)
+    
+    # 关闭音频流
+    if stream.is_active():
+        stream.stop_stream()
     stream.close()
-    stream = open_stream()
+    p.terminate()
 
-# 关闭音频流
-print("Stopping...")  # 提示停止
-if stream.is_active():
-    stream.stop_stream()
-stream.close()
-p.terminate()
+if __name__ == '__main__':
+    main()   
